a38bc01610331f7344ad2b93e9dc9cca10dce46b
farhad-ahmed <frhd.ahmd@gmail.com>
2020-01-24 01:44:33 -0600

    add delete functionality

--- src/data/structure/heap/Main.java
+++ src/data/structure/heap/Main.java
@@ -5,10 +5,11 @@ public class Main {
     public static void main(String[] args) {
 
         //testCase1();
-        testCase2();
+        //testCase2();
+        testCase3();
     }
 
-    private static void testCase1(){
+    private static void testCase1() {
         MaxHeap maxHeap = new MaxHeap();
 
         maxHeap.insert(2);
@@ -25,7 +26,7 @@ public class Main {
         maxHeap.insert(8);
     }
 
-    private static void testCase2(){
+    private static void testCase2() {
         MaxHeap maxHeap = new MaxHeap();
 
         maxHeap.insert(30);
@@ -37,5 +38,27 @@ public class Main {
         maxHeap.insert(6);
         maxHeap.insert(40);
         maxHeap.insert(35);
+    }
+
+    private static void testCase3() {
+        MaxHeap maxHeap = new MaxHeap();
+
+        maxHeap.insert(40);
+        maxHeap.insert(35);
+        maxHeap.insert(30);
+        maxHeap.insert(15);
+        maxHeap.insert(10);
+        maxHeap.insert(25);
+        maxHeap.insert(5);
+
+        System.out.println("Removed " + maxHeap.delete());
+        System.out.println("Removed " + maxHeap.delete());
+        System.out.println("Removed " + maxHeap.delete());
+        System.out.println("Removed " + maxHeap.delete());
+        System.out.println("Removed " + maxHeap.delete());
+        System.out.println("Removed " + maxHeap.delete());
+        System.out.println("Removed " + maxHeap.delete());
+        //System.out.println("Removed " + maxHeap.delete());
+
     }
 }
--- src/data/structure/heap/MaxHeap.java
+++ src/data/structure/heap/MaxHeap.java
@@ -13,6 +13,8 @@ public class MaxHeap {
         arr.add(Integer.MIN_VALUE);
     }
 
+    // worst-case for adding one element in the heap logn (base 2)
+    // for n elements O(n) = nlogn
     public void insert (Integer val){
 
         int index = arr.size();
@@ -24,29 +26,19 @@ public class MaxHeap {
         }
         else{
             arr.add(index, val);
-            parentIndex = getParentIndex(index);
+            parentIndex = index/2;
             if(arr.get(parentIndex) < arr.get(index)){
                 keepChecking = true;
             }
             while (keepChecking){
                 index = swap(parentIndex, index);
-                parentIndex = getParentIndex(index);
+                parentIndex = index/2;
                 if(parentIndex ==0 || arr.get(parentIndex)> arr.get(index)){
                     keepChecking = false;
                 }
             }
         }
         printArray();
-    }
-
-    private Integer getParentIndex(Integer index){
-
-        if(index%2==0){
-            return index/2;
-        }
-        else{
-            return (index-1)/2;
-        }
     }
 
     private Integer swap(Integer parentIndex, Integer childIndex){
@@ -63,5 +55,39 @@ public class MaxHeap {
         System.out.println();
     }
 
+    public Integer delete(){
 
+        Integer deletedRoot = arr.get(1);
+        boolean keepLooking = false;
+        Integer currentIndex = 1;
+
+        // copy the last element in the root
+        arr.set(1, arr.get(arr.size()-1));
+        //remove the last index;
+        arr.remove(arr.size()-1);
+        Integer largestChildIndex = findLargestChildIndex(currentIndex);
+        if(largestChildIndex >0 && arr.get(currentIndex) < arr.get(largestChildIndex)){
+            keepLooking = true;
+        }
+        while(keepLooking){
+            currentIndex = swap(largestChildIndex, currentIndex);
+            largestChildIndex = findLargestChildIndex(currentIndex);
+            if(largestChildIndex ==-1 || arr.get(currentIndex) > arr.get(largestChildIndex)){
+                keepLooking = false;
+            }
+        }
+
+        return deletedRoot;
+    }
+
+    // return the largest one
+    private Integer findLargestChildIndex(Integer parentIndex){
+
+        Integer arraySize = arr.size()-1;
+
+        if(parentIndex*2 > arraySize || (parentIndex*2)+1 > arraySize){
+            return -1;
+        }
+        return arr.get(parentIndex*2)>arr.get((parentIndex*2)+1)?parentIndex*2:(parentIndex*2)+1;
+    }
 }
